a016d672d7886328c4d7636ea406f7e6926c22b8
shohei0990 <redgrid22@gmail.com>
2023-03-04 15:49:11 +0900

    コメント20

--- pages/page_c.py
+++ pages/page_c.py
@@ -21,10 +21,9 @@ if "df_map" not in state:
 df_map_0 = state.df_map
 df_select = state.df
 
-
-df_select = df_select.rename(
+df_select0 = df_select.rename(
     columns={'家賃': '家賃[万円]', '面積': '面積[m^2]', '徒歩時間': '駅徒歩時間[分]', 'アクセス': '区'})
-df_select['区'] = "品川"
+df_select0['区'] = "品川"
 tx0 = ['家賃[万円]', '面積[m^2]', '築年数', '階数', '駅徒歩時間[分]', '区', '間取り']
 tx10 = ['面積[m^2]', '築年数', '階数', '駅徒歩時間[分]', '区', '間取り']
 select_columns5 = tx10
@@ -44,11 +43,13 @@ for column in pre0_df:
 
 pre_df[select_columns_num] = pre0_df
 
-df_select_comp = df_select[tx0]
+df_select_comp = df_select0[tx0]
 df_select_dummy = pre_df[tx0]
 df_select_dummy0 = df_select_dummy[:200]
 
 pd_recc0 = pd.concat([df_select_comp, df_select_dummy0])
+
+# -------------------------------------------------------------------------------------
 
 st.title("おススメ物件比較")
 st.markdown("---")
@@ -57,9 +58,10 @@ image_col, info_col, sele_col = st.columns([2, 3, 2], gap="medium")
 image_col.image(f"{df_map_0['画像']}", use_column_width=True)
 se90 = info_col.write(df_map_0)
 
+st.markdown("---")
 text_col5, gra_col5 = st.columns([1, 3], gap="medium")
 
-if text_col5.button('予測開始'):
+if text_col5.button('物件おススメ診断'):
     # 学習までのデータ準備
     test0 = pd_recc0  # 比較用
     test0[select_columns5] = test0[select_columns5].fillna(-99)
@@ -71,15 +73,30 @@ if text_col5.button('予測開始'):
 
     # グラフの可視化
     fig5 = px.scatter(x=y_test, y=pred,
-                      title="x軸：家賃[万円]　VS　　y軸：予測家賃[万円]",
+                      title="  x軸：家賃[万円]　VS　　y軸：予測家賃[万円]",
                       range_x=[0, 50],
                       range_y=[0, 50],
                       )
     gra_col5.plotly_chart(fig5, use_container_width=True)
-
     pred0 = pd.DataFrame(pred, columns=['pred'])
     y_test = y_test.reset_index(drop=True)
-    pd_recc = pd.concat([y_test, pred0], axis=1)
+    pd_recc = pd.concat([df_select0, pred0], axis=1)
+    pd_recc['お得度'] = pd_recc['pred'] - pd_recc['家賃[万円]']
+
+    # ----------------------------------------------
+
 
 else:
     st.write('予測・比較グラフを出すには、ボタンを押してね。')
+
+st.markdown("---")
+
+y0 = pd_recc[pd_recc['ID'] == df_map_0['ID']]['お得度']
+y1 = round(y0[0], 1)
+se90 = text_col5.subheader(f"家賃 {y1} 万円お得")
+
+pd_recc_comp = pd_recc[:df_select.shape[0]]
+pd_recc_comp = pd_recc_comp.sort_values(by="お得度", ascending=False)
+
+se90 = st.subheader("お得物件の表示")
+se90 = st.write(pd_recc_comp.head(5))
