2544dbf0a4673933cfc5a4fd2aa41f58d14a9fef
shohei0990 <redgrid22@gmail.com>
2023-03-04 14:12:46 +0900

    コメント18

--- pages/page_a.py
+++ pages/page_a.py
@@ -1,44 +1,169 @@
-import streamlit as st
+import streamlit as st                    # streamlit
+from streamlit_folium import st_folium    # streamlitでfoliumを使う
 import pandas as pd
+import requests
+import urllib
+from urllib.parse import urlencode
+import gspread
+from google.oauth2 import service_account
+from google.oauth2.service_account import Credentials
+from gspread_dataframe import get_as_dataframe
+from gspread_dataframe import set_with_dataframe
+from matplotlib import pyplot as plt
+import japanize_matplotlib
+
+
+japanize_matplotlib.japanize()
+#plt.rcParams['font.family'] = 'MS Gothic'
+
+# ページ設定
+st.set_page_config(
+    page_title="streamlit-foliumテスト",
+    page_icon="🗾",
+    layout="wide"
+)
 
 # セッションステートを取得
 state = st.session_state
 
-if "df" not in state:
-    state.df = pd.DataFrame()
+# googleスプレッドシートの認証 streamlit io　のシークレット活用
 
-st.title('賃貸一覧')
+
+def gsheet_read():
+    scopes = ['https://www.googleapis.com/auth/spreadsheets',
+              'https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"], scopes=scopes)
+    gc = gspread.authorize(credentials)
+
+    # googleスプレッドシートの読み込み
+    SP_SHEET_KEY = st.secrets.SP_SHEET_KEY.key  # スプレッドシートのキー
+    sh = gc.open_by_key(SP_SHEET_KEY)
+    SP_SHEET = 'ueno003'  # sheet名
+    worksheet = sh.worksheet(SP_SHEET)  # シートのデータ取得
+
+    # sampleデータの取得
+    pre_data = worksheet.get_all_values()
+    col_name = pre_data[0][:]
+    df_gs = pd.DataFrame(pre_data[1:], columns=col_name)
+
+    # 取得数字データのstr型から数字の型変換
+    select_columns_num = ['面積', '家賃', '敷金', '礼金',
+                          '管理費', '築年数', '構造', '階数', '徒歩時間']
+    pre0_df = df_gs[select_columns_num]
+    for column in pre0_df:
+        pre0_df[column] = pd.to_numeric(pre0_df[column], errors='coerce')
+    df_gs[select_columns_num] = pre0_df
+
+    return(df_gs)
+
+# 緯度・経度情報の取得
+
+
+def Map_info(x):
+    makeUrl = "https://msearch.gsi.go.jp/address-search/AddressSearch?q="
+    s_quote = urllib.parse.quote(x['アドレス'])
+    response = requests.get(makeUrl + s_quote)
+    try:
+        map_info_d = response.json()[0]["geometry"]["coordinates"]
+        #map_info_d = pd.DataFrame(map_info)
+        return map_info_d[0], map_info_d[1]
+    except Exception as e:
+        print(e)
+        return 0, 0
+
+
+df_final = pd.DataFrame()
+# 公開時に使用する。
+df_final = gsheet_read()
+
+# ローカルテスト用
+#df_final = pd.read_csv('realestateinfo_test _ueno003.csv')
+
+# 1. 画面の表示
+# サイドバー
+st.sidebar.title('MAP表示条件')
+xmax = st.sidebar.number_input('物件表示数 ：', 0, 1000, 100)
+ymax = st.sidebar.number_input('test ：', 0, 1000, 200)
+
+# メイン画面 検索
+st.title('賃貸検索')
+st.markdown("---")
+text_col, gra_col = st.columns([1, 1], gap="medium")
+text_col.subheader('絞り込み条件 : ')
+se1_min, se1_max = text_col.slider("● 家賃[万円] の 下限 ～ 上限", 0, 100, (3, 30))
+se2_min, se2_max = text_col.slider("● 面積[m^2] の 下限 ～ 上限 ", 0, 250, (5, 90))
+se3_min, se3_max = text_col.slider("● 駅徒歩[分] 以内", 0, 30, (0, 15))
+
+button_css = f"""
+<style>
+  div.stButton > button:first-child  {{
+    font-weight  : bold                ;/* 文字：太字                   */
+    border       :  5px solid #f36     ;/* 枠線：ピンク色で5ピクセルの実線 */
+    border-radius: 10px 10px 10px 10px ;/* 枠線：半径10ピクセルの角丸     */
+    background   : #ddd                ;/* 背景色：薄いグレー            */
+  }}
+</style>
+"""
+
+st.markdown("---")
+st.subheader('詳細検索 : ')
+extra_configs_1 = st.expander("詳細検索")  # Extra Configs
+with extra_configs_1:
+    se4_max = st.slider("● 築年数 以内", 0, 30, 15)
+#    se3 = st.multiselect(
+#        '間取り', ['ワンルーム', '1K', '1DK', '1LDK', '2DK', '2LDK'], ['2LDK'])
+#    se4 = st.multiselect('区', ['品川', '渋谷', '江戸川', '港'], ['品川'])
+#    se5 = st.multiselect('市町', ['南品川', '東五反田', '南大井', '東品川'], ['南品川'])
+
+# フィルタリング
+df_final0 = df_final
+joken1 = (df_final0["家賃"] > se1_min) & (df_final0["家賃"] < se1_max)
+df_final0 = df_final0[joken1]
+joken2 = (df_final0["面積"] > se2_min) & (df_final0["面積"] < se2_max)
+df_final0 = df_final0[joken2]
+joken3 = (df_final0["徒歩時間"] > se3_min) & (df_final0["徒歩時間"] < se3_max)
+df_final0 = df_final0[joken3]
+joken4 = (df_final0["築年数"] < se4_max)
+df_final0 = df_final0[joken4]
+#joken3 = df_final0["間取り"].isin(se3)
+#df_final0 = df_final0[joken3]
+#joken4 = df_final0["区"].isin(se4)
+#df_final0 = df_final0[joken4]
+
+japanize_matplotlib.japanize()
+gra_col.subheader(f"物件ヒット数 {df_final0.shape[0]}件")
+fig = plt.figure(figsize=(10, 5))
+plt.hist(df_final0["家賃"], bins=df_final0.shape[0]//10)
+plt.xlim([0, 50])
+plt.ylim([0, 50])
+plt.xlabel('家賃[万円]')
+plt.ylabel('物件数')
+# plt.title("物件数")
+gra_col.pyplot(fig)
+
+
+st.markdown(button_css, unsafe_allow_html=True)
+if text_col.button('検索実行'):
+    state.df = df_final0
+
 st.markdown("---")
 
-df_select = state.df
-df_select_image = df_select[:20]
+st.subheader('(フィルター後のデータ確認用)')
 
-st.markdown("---")
+se90 = st.write(df_final0)
+se91 = st.write(df_final0.shape)
 
-for i, row in df_select_image.iterrows():
-    image_col, info_col, sele_col = st.columns([2, 3, 2], gap="medium")
-    info_col.subheader(f"● {row['名称']}")
-    image_col.image(f"{row['画像']}", use_column_width=True)
+if df_final0.shape[0] > 50:
+    df = df_final0[:50]
+else:
+    df = df_final0
 
-    # st.write(m_data)
-    info_col.text(f"住所　：{row['アドレス']}")
-    info_col.text(f"家賃　：{row['家賃']}")
-    info_col.text(f"間取り：{row['間取り']}")
-    info_col.text(f"面積　：{row['面積']}")
-    info_col.text(f"最寄駅：{row['最寄駅']}")
-    info_col.text(f"最寄駅：{row['徒歩時間']}")
-    info_col.text(f"築年数：{row['築年数']}")
-    info_col.text(f"階数　：{row['階数']}")
-    info_col.text(f"構造　：{row['構造']}")
-    info_col.text(f"敷金　：{row['敷金']}")
-    info_col.text(f"礼金　：{row['礼金']}")
-    info_col.text(f"管理費：{row['管理費']}")
-    # info_col.text(f"オススメ度：{df_select['間取り']}")
+df_info = pd.DataFrame()
+df_info[['経度', '緯度']] = df.apply(lambda x: Map_info(x),
+                                 axis=1, result_type='expand')
 
-    if sele_col.button(f"MAP確認{i}"):
-        state.df_map = row
+df = pd.concat([df, df_info], axis=1)
 
-    st.markdown("---")
-
-se90 = st.write(state.df)
-se91 = st.write(state.df.shape)
+se90 = st.write(df)
+se91 = st.write(df.shape)
---
+++ pages/page_c.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from matplotlib import pyplot as plt
+import japanize_matplotlib
+from joblib import load
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error as MSE
+
+japanize_matplotlib.japanize()
+
+# セッションステートを取得
+state = st.session_state
+
+if "df" not in state:
+    state.df = pd.DataFrame()
+
+if "df_map" not in state:
+    state.df_map = pd.DataFrame()
+
+df_map_0 = state.df_map
+df_select = state.df
+
+
+df_select = df_select.rename(
+    columns={'家賃': '家賃[万円]', '面積': '面積[m^2]', '徒歩時間': '駅徒歩時間[分]', 'アクセス': '区'})
+df_select['区'] = "品川"
+tx0 = ['家賃[万円]', '面積[m^2]', '築年数', '階数', '駅徒歩時間[分]', '区', '間取り']
+tx10 = ['面積[m^2]', '築年数', '階数', '駅徒歩時間[分]', '区', '間取り']
+select_columns5 = tx10
+
+################################################################################################################################################
+# csvファイルの読み込み
+# pd.DataFrame(pre_data[1:], columns=col_name)  # 一段目をカラム、以下データフレームで取得
+pre_df = pd.read_csv("realestateinfo_test2_ishida91.csv")
+pre_df = pre_df.rename(columns={
+                       '面積': '面積[m^2]', '家賃': '家賃[万円]', '敷金': '敷金[万円]', '礼金': '礼金[万円]', '駅徒歩時間': '駅徒歩時間[分]'})
+select_columns_num = ['面積[m^2]', '家賃[万円]', '築年数', '階数', '駅徒歩時間[分]']
+pre0_df = pre_df[select_columns_num]
+
+# object →　数字の変換 新しくスプシから取得する際には全てObjectになっているため。
+for column in pre0_df:
+    pre0_df[column] = pd.to_numeric(pre0_df[column], errors='coerce')
+
+pre_df[select_columns_num] = pre0_df
+
+df_select_comp = df_select[tx0]
+df_select_dummy = pre_df[tx0]
+df_select_dummy0 = df_select_dummy[:200]
+
+pd_recc0 = pd.concat([df_select_comp, df_select_dummy0])
+
+st.title("おススメ物件比較")
+st.markdown("---")
+
+image_col, info_col, sele_col = st.columns([2, 3, 2], gap="medium")
+image_col.image(f"{df_map_0['画像']}", use_column_width=True)
+se90 = info_col.write(df_map_0)
+
+text_col5, gra_col5 = st.columns([1, 3], gap="medium")
+
+if text_col5.button('予測開始'):
+    # 学習までのデータ準備
+    test0 = pd_recc0  # 比較用
+    test0[select_columns5] = test0[select_columns5].fillna(-99)
+    X_test, y_test = pd.get_dummies(test0[select_columns5]), test0['家賃[万円]']
+
+    # 評価データに対する予測を行い、その結果を変数predに代入してください。
+    rf = load('rf_model-TK-slim.joblib2.cmp')
+    pred = rf.predict(X_test)
+
+    # グラフの可視化
+    fig5 = px.scatter(x=y_test, y=pred,
+                      title="x軸：家賃[万円]　VS　　y軸：予測家賃[万円]",
+                      range_x=[0, 50],
+                      range_y=[0, 50],
+                      )
+    gra_col5.plotly_chart(fig5, use_container_width=True)
+
+    pred0 = pd.DataFrame(pred, columns=['pred'])
+    y_test = y_test.reset_index(drop=True)
+    pd_recc = pd.concat([y_test, pred0], axis=1)
+
+else:
+    st.write('予測・比較グラフを出すには、ボタンを押してね。')
